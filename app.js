73009e9b0eab99749817faf6fe76ca7bcd9e6995
Keniola2 <eniolahabeeb98@gmail.com>
2023-08-06 12:12:21 -1100

    a fashion store ecommerce website built with vanilla js

---
+++ app.js
@@ -0,0 +1,243 @@
+const productEl = document.querySelector(".products");
+const cartItemsEl = document.querySelector(".cart-items");
+const subtotalEl = document.querySelector('.subtotal');
+const totalItemsInCartEl = document.querySelectorAll('.total-items-in-cart');
+const shirtsEl = document.querySelector(".shirts");
+const pantsEl = document.querySelector(".pants");
+const jacketsEl = document.querySelector(".jackets");
+const button=document.querySelectorAll(".btn")
+
+
+function renderProducts(){
+    if (window.focus=productEl) {
+        shirts.forEach(shirt=>{
+            productEl.innerHTML +=`
+            <div class="product">
+          <div id="carouselExampleSlidesOnly" class="carousel slide slide-size" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="${shirt.img1}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="${shirt.img2}" class="d-block w-100" alt="...">
+              </div>
+            </div>
+          </div>
+    
+          <a href="#"><p><strong>${shirt.name}</strong></p></a>
+          <div class="pdetails">
+            <div class="price">$${shirt.price}</div>
+            <div class="size">
+            <select>
+            <option>${shirt.size[0]}</option>
+            <option>${shirt.size[1]}</option>
+            <option>${shirt.size[2]}</option>
+            </select>
+            </div>
+            <button class="btn"  onclick="addToCart(${shirt.id})">ADD TO CART</button>
+          </div>
+        </div>`
+        })
+        
+    } if (window.focus=pantsEl) { pants.forEach(pant=>{
+        pantsEl.innerHTML +=`<div class="pant">
+        <div id="carouselExampleSlidesOnly" class="carousel slide slide-size" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="${pant.img1}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="${pant.img2}" class="d-block w-100" alt="...">
+            </div>
+          </div>
+        </div>
+  
+        <a href="#"><p><strong>${pant.name}</strong></p></a>
+        <div class="pdetails">
+          <div class="price">$${pant.price}</div>
+          <div class="size">
+          <select>
+          <option>${pant.size[0]}</option>
+          <option>${pant.size[1]}</option>
+          <option>${pant.size[2]}</option>
+          </select>
+          </div>
+          <button class="btn" onclick="addToCart(${pant.id})" >ADD TO CART</button>
+        </div>
+      </div>`
+    })
+        
+    }
+    if (window.focus=jacketsEl) { jackets.forEach(jacket=>{
+        jacketsEl.innerHTML +=`<div class="jacket">
+        <div id="carouselExampleSlidesOnly" class="carousel slide slide-size" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="${jacket.img1}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="${jacket.img2}" class="d-block w-100" alt="...">
+            </div>
+          </div>
+        </div>
+  
+        <a href="#"><p><strong>${jacket.name}</strong></p></a>
+        <div class="pdetails">
+          <div class="price">$${jacket.price}</div>
+          <div class="size">
+          <select>
+            <option >${jacket.size[0]}</option>
+            <option>${jacket.size[1]}</option>
+            <option>${jacket.size[2]}</option>
+          </select>
+          </div>
+          <button class="btn" onclick="addToCart(${jacket.id})">ADD TO CART</button>
+        </div>
+      </div>`
+    })
+        
+    }
+   
+}
+
+
+
+ const cart = [];
+
+ function addToCart(id) {
+  let cartData = JSON.parse(localStorage.getItem('cart')) || [];
+
+  // Check if the item already exists in the cart
+  if (cartData.some((item) => item.id === id)) {
+    alert("Product already in cart");
+  } else {
+    // Find the item to add based on the provided ID
+    const item = shirts.find((shirt) => shirt.id === id) || pants.find((pant) => pant.id === id) || jackets.find((jacket) => jacket.id === id);
+
+    // Add the item to the cart with an initial quantity of 1
+    cartData.push({ ...item, numberOfUnits: 1,});
+
+    // Update the cart data in localStorage
+    localStorage.setItem('cart', JSON.stringify(cartData));
+  }
+  
+  alert('Added to cart');
+  updateCart();
+}
+
+
+
+  function increaseNumberOfUnit(itemId){
+    const cartData =JSON.parse(localStorage.getItem('cart')) || [];
+
+    const item =cartData.find((item)=>item.id===itemId)
+    if (item) {
+      item.numberOfUnits++;
+      localStorage.setItem('cart',JSON.stringify(cartData));
+//////////////////// update the number element for the corresponding item
+const numberElement =document.querySelector(`.number[data-item-id="${itemId}"]`);
+if (numberElement) {
+  numberElement.textContent = item.numberOfUnits;
+}   
+    } 
+   updateCart() 
+  }
+
+  function decreaseNumberOfUnit(itemId){
+    const cartData =JSON.parse(localStorage.getItem('cart')) || [];
+
+    const item =cartData.find((item)=>item.id===itemId)
+    if (item) {
+      if (item.numberOfUnits>0) {
+        item.numberOfUnits--;
+        localStorage.setItem('cart',JSON.stringify(cartData));
+        /////////////////
+        const numberElement = document.querySelector(`.number[data-item-id="${itemId}"]`);
+if (numberElement) {
+  numberElement.textContent = item.numberOfUnits;
+}
+
+      }
+    } 
+ updateCart()
+  }
+
+  
+
+  function renderSubtotal() {
+    const cartData = JSON.parse(localStorage.getItem('cart')) || [];
+    let totalPrice = 0;
+    let totalItem = 0;
+  
+    cartData.forEach((item) => {
+      totalPrice += item.price * item.numberOfUnits;
+      totalItem += item.numberOfUnits;
+    });
+  
+    subtotalEl.innerHTML = `Subtotal (${totalItem} items): $${totalPrice.toFixed(2)}`;
+  
+    const totalItemsInCartElements = document.querySelectorAll('.total-items-in-cart');
+    
+    totalItemsInCartElements.forEach((element) => {
+      element.innerHTML= `CART (${totalItem})`;
+    });
+  }
+////////render cart items///////////////////
+  function renderCartItems(){
+    const cartData = JSON.parse(localStorage.getItem('cart')) || [];
+   cartItemsEl.innerHTML = '';
+      // Render the cart items
+      cartData.forEach((item,index) => {
+        console.log(item)
+        cartItemsEl.innerHTML += `
+          <div class="cart-item">
+            <div class="item-info">
+              <img src="${item.img1}" alt="">
+              <p class="item-name">${item.name} <small id="smz"></small></p>
+              <div class="remove" onclick="removeCartItem(${item.id})">
+              <p>Remove</P>
+              </div>
+            </div>
+            <div class="unit-price">
+              <small>$</small>${item.price}
+            </div>
+            <div class="units">
+              <div class="btn minus" onclick="decreaseNumberOfUnit(${item.id})"><p>-</p></div>
+              <div class="number" data-item-id="${item.id}">${item.numberOfUnits}</div>
+              <div class="btn plus" onclick="increaseNumberOfUnit(${item.id})"><P>+</P></div>
+            </div>
+          </div>`;
+      });
+    }
+
+    function removeCartItem(itemId) {
+      let cartData = JSON.parse(localStorage.getItem('cart')) || [];
+    
+      // Filter out the item with the matching ID
+      cartData = cartData.filter((item) => item.id !== itemId);
+    
+      localStorage.setItem('cart', JSON.stringify(cartData));
+      updateCart();
+     // alert('Removed from cart');
+    }
+    
+    function updateCart(){
+  renderCartItems()
+  renderSubtotal()
+      }
+    renderProducts()
+    updateCart()
+
+    function showMenu(params) {
+      document.querySelector(".menu").style.display='flex'
+      
+    }
+
+function hideMenu(params) {
+  document.querySelector(".menu").style.display='none'
+  
+}
+
+
+
+
---
+++ store.js
@@ -0,0 +1,267 @@
+const shirts = [
+    {
+      id: 0,
+      name: "Embroidered Beads Cotton T-shirt",
+      price: 29.99,
+      size:[
+       'small',
+      'medium',
+       "large"
+      ] ,
+      img1: "./image/louis-vuitton-embroidered-beads-cotton-t-shirt-Front view.jpg",
+      img2: "./image/louis-vuitton-embroidered-beads-cotton-t-shirt-worn view.jpg",
+    },
+    {
+      id: 1,
+      name: "Multi Tools Embroidered T-shirt",
+      price: 24.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1: "./image/louis-vuitton-lv-multi-tools-embroidered-t-shirt-Front view.jpg",
+      img2:"./image/louis-vuitton-lv-multi-tools-embroidered-t-shirt-Worn view.jpg",
+    },
+    {
+      id: 2,
+      name: "Pumpkins Printed T-shirt",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-lv-x-yk-pumpkins-printed-t-shirt-Front view.jpg",
+      img2: "./image/louis-vuitton-lv-x-yk-pumpkins-printed-t-shirt-Worn view.jpg",
+    },
+    {
+      id: 3,
+      name: "Jazz Flyers T-shirt",
+      price: 25.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-lv-jazz-flyers-short-sleeved-t-shirt-Front view.jpg",
+      img2: "./image/louis-vuitton-lv-jazz-flyers-short-sleeved-t-shirt-Worn view.jpg",
+    },
+    {
+      id: 4,
+      name: "Monogram Sporty V Neck",
+      price: 79.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-monogram-sporty-v-neck-t-shirt-Front view.jpg",
+      img2: "./image/louis-vuitton-monogram-sporty-v-neck-t-shirt-Worn view.jpg",
+    },
+    {
+      id: 5,
+      name: "Vintage Cycling Polo",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-vintage-cycling-polo-Front view.jpg",
+      img2: "./image/louis-vuitton-vintage-cycling-polo-Worn view.jpg",
+    },
+     {
+      id: 6,
+      name: "Half Zipped Cotton Sweatshirt",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-printed-half-zipped-cotton-sweatshirt--HPY14WZ77C65_PM2_Front view.jpg",
+      img2: "./image/louis-vuitton-printed-half-zipped-cotton-sweatshirt-Worn view.jpg",
+    },
+    
+  ];
+  
+
+  // /////////////////////////////////////////////pants pants    //////////////////////////////////////////////////
+
+
+ const pants = [
+    {
+      id: 7,
+      name: "Cotton Blend Tailored Shorts",
+      price: 29.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1: "./image/louis-vuitton-cotton-blend-tailored-shorts-Front view.jpg",
+      img2: "./image/louis-vuitton-cotton-blend-tailored-shorts-Worn view.jpg",
+    },
+    {
+      id: 8,
+      name: "Denim Carpenter Shorts",
+      price: 24.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1: "./image/louis-vuitton-denim-carpenter-shorts-Front view.jpg",
+      img2:"./image/louis-vuitton-denim-carpenter-shorts-Worn view.jpg",
+    },
+    {
+      id: 9,
+      name: "Ripstop Tailored Pants",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-ripstop-tailored-pants-Front view.jpg",
+      img2: "./image/louis-vuitton-ripstop-tailored-pants-Worn view.jpg",
+    },
+    {
+      id: 10,
+      name: "Strappy Pants",
+      price: 25.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-strappy-pants-Front view.jpg",
+      img2: "./image/louis-vuitton-strappy-pants-Worn-view.jpg",
+    },
+    {
+      id: 11,
+      name: "Printed Denim Pants",
+      price: 79.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ],
+      img1:"./image/louis-vuitton-printed-denim-pants-Front view.jpg",
+      img2: "./image/louis-vuitton-printed-denim-pants-Worn view.jpg",
+    },
+    {
+      id: 12,
+      name: "Perforated Leather Shorts",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ],
+      img1:"./image/louis-vuitton-perforated-leather-shorts-Front view.jpg",
+      img2: "./image/louis-vuitton-perforated-leather-shorts-Worn view.jpg",
+    },
+     {
+      id: 13,
+      name: "Embroidered Faces Cargo Pants",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ],
+      img1:"./image/louis-vuitton-lv-x-yk-embroidered-faces-cargo-pants-Front view.jpg",
+      img2: "./image/louis-vuitton-lv-x-yk-embroidered-faces-cargo-pants-Worn view.jpg",
+    },
+  ];
+
+  //////////////////////////jACkets and blazers//////////////////////////////////////////////
+  
+  const jackets = [
+    {
+      id: 14,
+      name: "Cotton Coach Jacket",
+      price: 29.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1: "./image/louis-vuitton-cotton-coach-jacket-Front view.jpg",
+      img2: "./image/louis-vuitton-cotton-coach-jacket-Worn view.jpg",
+    },
+    {
+      id: 15,
+      name: "Embroidered Evening Pont Neuf Jacket",
+      price: 24.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1: "./image/louis-vuitton-embroidered-evening-pont-neuf-jacket-Front view.jpg",
+      img2:"./image/louis-vuitton-embroidered-evening-pont-neuf-jacket-Worn view.jpg",
+    },
+    {
+      id: 16,
+      name: "Jet Pocket Blouson Jacket",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-jet-pocket-blouson-Front view.jpg",
+      img2: "./image/louis-vuitton-jet-pocket-blouson-Worn view.jpg",
+    },
+    {
+      id: 17,
+      name: "Landscape Denim Jacket",
+      price: 25.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-landscape-denim-jacket-Front view.jpg",
+      img2: "./image/louis-vuitton-landscape-denim-jacket-Worn view.jpg",
+    },
+    {
+      id: 18,
+      name: "Leather Cafe Racer Jackets",
+      price: 79.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-leather-cafe-racer-Front view.jpg",
+      img2: "./image/louis-vuitton-leather-cafe-racer-Worn view.jpg",
+    },
+    {
+      id: 19,
+      name: "Leather Mix Biker Jacket",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-leather-mix-biker-Front view.jpg",
+      img2: "./image/louis-vuitton-leather-mix-biker-Worn view.jpg",
+    },
+     {
+      id: 20,
+      name: "Martingale Jacket",
+      price: 39.99,
+      size:[
+        'small',
+       'medium',
+        "large"
+       ] ,
+      img1:"./image/louis-vuitton-wrapped-martingale-jacket-front view.jpg",
+      img2: "./image/louis-vuitton-wrapped-martingale-jacket-Worn view.jpg",
+    },
+  ];
